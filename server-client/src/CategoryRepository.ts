63e47d0dd6d91efba040a9f0b1b7acf250f34735
psu6510110124 <Discoboi0470@gmail.com>
2022-12-15 13:48:42 +0700

    STEP1 Frontend  category repo with getAll

---
+++ server-client/src/CategoryRepository.ts
@@ -0,0 +1,17 @@
+export interface IRepository<T>{
+    getAll():Promise<T[] | null>;
+}
+export default interface category {
+    id: number;
+    title: string;
+}
+export class CategoryRepository implements IRepository<category> {
+    async getAll(): Promise<category[] | null> {
+        return [
+            {
+                id:1, title:'Romance',
+                
+            }
+        ]
+    }
+}
