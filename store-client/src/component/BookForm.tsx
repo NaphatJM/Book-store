4fe0299c8de36a4729a089e96164c2cb697d944e
psu6510110124 <Discoboi0470@gmail.com>
2022-12-17 10:43:59 +0700

    [fix] install dependencies and book fixtures

--- store-client/src/component/BookForm.tsx
+++ store-client/src/component/BookForm.tsx
@@ -6,7 +6,7 @@ interface Props {
   book: Partial<Book>,
   categoryList: Category[],
   callbackFn: (book: Partial<Book>) => void
-
+  
 }
 
 function BookForm(props: Props) {
@@ -17,53 +17,44 @@ function BookForm(props: Props) {
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const title = titleRef.current?.value
-    if (title && title.length > 5) {
-      props.callbackFn({
-        id: props.book.id,
-        title: titleRef.current?.value,
-        price: Number(priceRef.current?.value),
-        stockAmount: Number(stockAmountRef.current?.value),
-        category: {
-          id: Number(categoryRef.current?.value)
-        }
-      })
-    }else{
-      alert("Yout Title are shorter than 5 letters")
-      }
-    }
 
-    return (
-      <div>
-        <form onSubmit={onSubmit}>
-  
-          <div>
-            Title : <input type="text" defaultValue={props.book.title} ref={titleRef} required/>
-          </div>
-  
-          <div>
-            Price : <input type="number" step="0.01" min="0" defaultValue={props.book.price} ref={priceRef} />
-          </div>
-  
-          <div>
-            Stock Amount : <input type="number" min="0" defaultValue={props.book.stockAmount} ref={stockAmountRef} />
-          </div>
-  
-          <div>
-            Category :
-            <select defaultValue={props.book.category?.id} ref={categoryRef}>
-              <option hidden></option>
-              {props.categoryList.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
-            </select>
-          </div>
-  
-          <button type="submit">Submit</button>
-  
-        </form>
-      </div>
-  
-    );
+    props.callbackFn({
+      id: props.book.id,
+      title: titleRef.current?.value,
+      price: Number(priceRef.current?.value),
+      stockAmount: Number(stockAmountRef.current?.value),
+      category: {
+        id: Number(categoryRef.current?.value)
+      }
+    })
+    
   }
-  
-  export default BookForm;
-  
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          Title : <input type="text" defaultValue={props.book.title} ref={titleRef} required/>
+        </div>
+        <div>
+          Price : <input type="number" step="0.01" min="0" defaultValue={props.book.price} ref={priceRef} />
+        </div>
+        <div>
+          Stock Amount : <input type="number" min="0" defaultValue={props.book.stockAmount} ref={stockAmountRef} />
+        </div>
+
+        <div>
+          Category :
+          <select defaultValue={props.book.category?.id} ref={categoryRef}>
+            <option hidden></option>
+            {props.categoryList.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
+          </select>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+
+  );
+}
+
+export default BookForm;
--- store-server/src/fixtures.ts
+++ store-server/src/fixtures.ts
@@ -7,13 +7,15 @@ async function loadFixtures(clearData = false){
         await db('category').del()
     }
     await db.batchInsert('category', [
-        {id: 1, title: 'Computer'},
-        {id: 2, title: 'Fantasy'}
+        {id: 1, title: 'Romance'},
+        {id: 2, title: 'Fantasy'},
+        {id: 3, title: 'Sci-fi'}
     ])
     await db.batchInsert('book', [
-        { id: 1, title: 'Harry Potter', price: 560, stockAmount: 10, categoryId: 1 },
-        { id: 2, title: 'Game of Thrones', price: 520, stockAmount: 6, categoryId: 1 },
-        { id: 3, title: 'Node.js', price: 300, stockAmount: 4, categoryId: 2 },
+        { id: 1, title: 'Percy jackson', price: 490, stockAmount: 10, categoryId: 2 },
+        { id: 2, title: 'Interstella', price: 299, stockAmount: 6, categoryId: 3 },
+        { id: 3, title: 'Titanic', price: 399, stockAmount: 4, categoryId: 1 },
+        { id: 4, title: 'Fantastic beast', price: 490, stockAmount: 7, categoryId: 2}
     ])
 }
 export default loadFixtures
